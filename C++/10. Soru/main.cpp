7ef9e9fe8d8f5c4a7cf0ededf4bac7b4c8950c88
eray-c <73905858+eray-c@users.noreply.github.com>
2020-12-13 21:11:21 +0300

    Update main.cpp

--- C++/10. Soru/main.cpp
+++ C++/10. Soru/main.cpp
@@ -1,24 +1,43 @@
-#include <cstdlib>
 #include <iostream>
-
-int asalMi(int );
+#include <ctime>
 
 using namespace std;
+
+int asalMi(int );
+int my_strlen(string dizi);
+
 int main(int argc, char** argv) {
-    /*10.Soru C++ Versiyon
-     * ASCII degeri asal olan harfleri ekrana yazdirir.*/
+   
+    srand(time(NULL));
     char buyuk[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char kucuk[27] = "abcdefghijklmnopqrstuvwxyz";
-    int i,a,b;
-    for(i=0,a=65,b=97; i<26; i++,a++,b++) {
+    string depo;
+    int i=0,j=0,a=65,b=97;
+    while(i<26) {
         if(asalMi(a)==0) {
-            cout <<kucuk[i]<<endl;
+            depo[j] = buyuk[i];
+            j++;
         }
         if(asalMi(b)==0) {
-            cout <<buyuk[i]<<endl;
+            depo[j] = kucuk[i];
+            j++;
         }
-    }    
-   
+        i++;
+        a++;
+        b++;
+    }
+    
+    char dizi[20][3];
+    int k,l;
+    for(k=0; k<20; k++) {
+        for(l=0; l<3; l++) {
+            int sayac = rand() % my_strlen(depo);
+            dizi[k][l] = depo[sayac];
+            cout<<dizi[k][l];    
+        }
+        cout<<" "<<endl;
+    }
+    
     return 0;
 }
 int asalMi(int sayi) {
@@ -33,4 +52,12 @@ int asalMi(int sayi) {
         return 0;
     } else
         return 1;
+}
+int my_strlen(string dizi) {
+    int i = 0;
+    
+    while(dizi[i] != '\0') {
+       i++;
+    }
+    return i;
 }
